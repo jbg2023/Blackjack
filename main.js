cd7f3bdb2d1a4cc86079609520b14b463dcedbb3
jbg2023 <jbgates@protonmail.com>
2022-11-17 16:37:14 -0800

    function to create deck of 52 cards in an array, card object has cardValue, suit and score values

--- main.js
+++ main.js
@@ -24,6 +24,24 @@ hitBtn.addEventListener('click', hitMe());
 stayBtn.addEventListener('click', stay());
 
   /*----- functions -----*/
+  function makeDeck() {
+    deck = [];
+    //iterate through suit and value arrays to push each card into deck array
+    //pair each card value with a suit
+    for (let i = 0; i < cardValue.length; i++) {
+      for (let j = 0; j < suit.length; j++) {
+        let score = parseInt(cardValue[i]);
+        if (cardValue[i] == 'J' || cardValue[i] == 'Q' || cardValue[i] == 'K')
+            score = 10;
+        if (cardValue[i] == 'A')
+            score = 11;
+        //array has keys for value and suit of each card
+        let card = {Value: cardValue[i], Suit: suit[j], Score: score};
+        deck.push(card);
+      } 
+    } return deck;
+}
+
 function init() {
 
 }
@@ -33,5 +51,5 @@ function hitMe() {
 }
 
 function stay() {
-  
+
 }
