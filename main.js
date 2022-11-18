36411860195cb2e929f10490c63e1746f861fefb
jbg2023 <jbgates@protonmail.com>
2022-11-17 16:05:54 -0800

    Arrays for suits and cards

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
   /*----- constants -----*/
+let suit = ['♦', '♥', '♣', '♠']
+let cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-
-  /*----- state variables -----*/
+/*----- state variables -----*/
 
 
   /*----- cached elements  -----*/
@@ -11,4 +12,3 @@
 
 
   /*----- functions -----*/
-  console.log('hi')
