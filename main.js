2d04d1160d2e1bb1e02ac67f4e649dde9556fd83
jbg2023 <jbgates@protonmail.com>
2022-11-17 16:11:58 -0800

    game state variables

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ let suit = ['♦', '♥', '♣', '♠']
 let cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 /*----- state variables -----*/
-
+let deck = [];
+let dealerHand = [];
+let playerHand = [];
+let gameStart = false;
+let gameOver = false;
+let winner = false;
+let dealerScore = 0;
+let playerScore = 0;
 
   /*----- cached elements  -----*/
 let display = document.getElementById('display');
