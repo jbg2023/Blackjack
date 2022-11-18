43ce54fc24b42ecc2b99625287a15200fbde4b40
jbg2023 <jbgates@protonmail.com>
2022-11-17 16:08:50 -0800

    cached DOM elements

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ let cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A
 
 
   /*----- cached elements  -----*/
-
+let display = document.getElementById('display');
+let newGame = document.getElementById('newBtn');
+let hitBtn = document.getElementById('hitBtn');
+let stayBtn = document.getElementById('stayBtn');
 
   /*----- event listeners -----*/
 
